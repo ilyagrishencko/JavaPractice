6be967a16a4fa4c402ed90019aff0ebeac4b33f5
ilyagrishencko <ilya.grishencko2018@yandex.ru>
2025-11-28 23:29:23 +0300

    Update and rename Pr1.java to DefaultAccessExample.java

---
+++ 11 Ограничение доступа/DefaultAccessExample.java
@@ -0,0 +1,12 @@
+package animals;
+
+// Класс с доступом по умолчанию (без спецификатора)
+class DefaultAccessClass {
+    // Поле с доступом по умолчанию
+    String defaultField = "Это поле с доступом по умолчанию";
+    
+    // Метод с доступом по умолчанию
+    void defaultMethod() {
+        System.out.println("Это метод с доступом по умолчанию");
+    }
+}
