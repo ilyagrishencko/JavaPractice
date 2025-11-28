eee5e303301737fcca4c81e71342377ee699d729
ilyagrishencko <ilya.grishencko2018@yandex.ru>
2025-11-28 20:09:21 +0300

    Update Pr1.java

--- Консольный ввод – вывод/Pr1.java
+++ Консольный ввод – вывод/Pr1.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+/*
+ Практика #1
+ Напишите программу, которая просит ввести имя и выводит на консоль строку "Привет, <введенное имя>".
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите ваше имя: ");
+        String name = scanner.next();
+        System.out.println("Привет, " + name);
+        scanner.close();
+    }
+}
