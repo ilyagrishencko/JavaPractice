2c00193dcae02e94922bd3e9e710591fcfd0479b
ilyagrishencko <ilya.grishencko2018@yandex.ru>
2025-11-28 20:19:38 +0300

    Update and rename Pr1 to Pr1.java

---
+++ 3Базовые типы данных и литералы/Pr1.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    /**
+     Практика #1
+     Приведите примеры для всех типов данных. Примеры должны также включать нулевые значения.
+     */
+    public static void main(String[] args) {
+        byte nonZeroByte = 127;
+        byte zeroByte = 0;
+        short nonZeroShort = 32767;
+        short zeroShort = 0;
+        int nonZeroInt = 2147483647;
+        int zeroInt = 0;
+        long nonZeroLong = 9223372036854775807L;
+        long zeroLong = 0L;
+        float nonZeroFloat = 2.72f;
+        float zeroFloat = 0.0f;
+        double nonZeroDouble = 0.57721;
+        double zeroDouble = 0.0;
+        char nonZeroChar = 'F';
+        char zeroChar = '\u0000';
+        boolean nonZeroBoolean = true;
+        boolean zeroBoolean = false;
+    }
+}
